c2d957352e82addd18978d3bc8fdb3d72c71e8df
Marc Görtz <mail@marcgoertz.de>
2017-08-09 23:14:31 +0200

    O hai `READ`.

---
+++ src/squared.f
@@ -0,0 +1,7 @@
+      PROGRAM squared
+      INTEGER :: i
+      WRITE (*,*) "Please enter a number."
+      READ (*,*) i
+      WRITE (*,*) "Your number squared is ", i*i
+      STOP
+      END PROGRAM
